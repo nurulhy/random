87ea5aa21acad7d39830ede8d57da72e355d149c
nurulhy <33342511+nurulhy@users.noreply.github.com>
2018-02-28 20:14:01 +0700

    Update random.js

--- random.js
+++ random.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/02/25/influenza-flu/"}
 if(x=="2"){xxx="/2018/02/25/human-need-rest-and-sleep/"}a_to_ve[i].href="http://rekosite.epizy.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
