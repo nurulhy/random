382f3736c1b56d5224cd4c0bd34e8141529ac2e0
nurulhy <33342511+nurulhy@users.noreply.github.com>
2018-03-01 17:08:21 +0700

    Update random.js

--- random.js
+++ random.js
@@ -21,8 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/02/25/influenza-flu/"}
-if(x=="2"){xxx="/2018/02/25/human-need-rest-and-sleep/"}a_to_ve[i].href="http://rekosite.epizy.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/influenza-flu/"}
+if(x=="2"){xxx="/human-need-rest-and-sleep/"}
+if(x=="1"){xxx="/become-genius-with-foods/"}
+if(x=="2"){xxx="/bunch-yellow-colored/"}a_to_ve[i].href="http://rekosite.epizy.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
