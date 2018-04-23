8fc842d67a57dd3f7fc8dbadb2d6597ae1dc78a9
nurulhy <33342511+nurulhy@users.noreply.github.com>
2018-04-23 09:07:01 +0700

    Update randomnya rekoklik.js

--- randomnya rekoklik.js
+++ randomnya rekoklik.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/p/humans-will-definitely-feel-thirsty-and.html"}
 if(x=="2"){xxx="/p/the-basic-need-for-human-beings-is.html"}
-if(x=="3"){xxx="/p/how-to-remove-acnes-scar.html"}a_to_ve[i].href="http://rekoklik.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/p/how-to-remove-acnes-scar.html"}a_to_ve[i].href="https://rekoklik.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
