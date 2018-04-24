ca53090f53932e6f93f83e18c7112d8207f95605
nurulhy <33342511+nurulhy@users.noreply.github.com>
2018-04-24 13:21:33 +0700

    Update randomnya rekoklik.js

--- randomnya rekoklik.js
+++ randomnya rekoklik.js
@@ -23,8 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/02/how-to-remove-acnes-scar.html"}
 if(x=="2"){xxx="/2018/02/basic-human-needs-with-its.html"}
-if(x=="3"){xxx="/2017/07/manneedssolitude.html"}
-if(x=="4"){xxx="/2018/04/benefits-of-soursop-fruit.html"}a_to_ve[i].href="https://rekoklik.blogspot.co.id"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2017/07/manneedssolitude.html"}a_to_ve[i].href="https://rekoklik.blogspot.co.id"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
